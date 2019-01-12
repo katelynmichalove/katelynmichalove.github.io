04762ab0df7682cd8b3b6f7704565f53c0ca6664
Katelyn Michalove <katelynmichalove@gmail.com>
2019-01-11 18:10:48 -0800

    Added About Page CSS

---
+++ script.js
@@ -0,0 +1,20 @@
+
+function myFunction(x) {
+  x.classList.toggle("change");
+}
+
+function myMenuFunction() {
+  var x = document.getElementById("menu_links");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+$(function() {
+  $('a[href*=#]').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+  });
+});
